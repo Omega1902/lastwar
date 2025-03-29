e371219a34477351ec1d43a54ae3f6091a1dd87f
Omega1902 <27062486+Omega1902@users.noreply.github.com>
2025-03-29 18:13:41 +0100

    adding effective time

--- buffs.py
+++ buffs.py
@@ -1,4 +1,11 @@
+#!/usr/bin/env python3
 import math
+from collections.abc import Callable
+from functools import partial
+
+from prettytable import PrettyTable
+
+ALLIANCE_HELP_PERCENTAGE = 0.005  # 0.5 %
 
 
 def seconds_to_string(seconds: int) -> str:
@@ -9,9 +16,7 @@ def seconds_to_string(seconds: int) -> str:
     result = ""
     if days:
         result += f"{days}d "
-    if days or hours:
-        result += f"{hours}:"
-    return result + f"{minutes:02}:{seconds:02}"
+    return result + f"{hours}:{minutes:02}:{seconds:02}"
 
 
 def time_to_seconds(days: int, hours: int, minutes: int, seconds: int) -> int:
@@ -22,26 +27,68 @@ def format_percent(percentage: float) -> str:
     return f"{percentage * 100:.1f} %"
 
 
-def calc_duration(original_seconds: int, buffs: int) -> None:
-    print("Original time: " + seconds_to_string(original_seconds))
+def effective_time(construction: int, alliance_help_max: int, allinace_help_minimum_time: int, time: int) -> int:
+    time = max(0, time - construction)
+    for _ in range(alliance_help_max):
+        time -= max(0, allinace_help_minimum_time, math.floor(time * ALLIANCE_HELP_PERCENTAGE))
+    return max(0, time)
+
+
+def gen_row(title: str, time: int, make_effective: Callable[[int], int], relative: int | None) -> list[str]:
+    effective_time = make_effective(time)
+    if relative is None:
+        return [title, seconds_to_string(time), "", "", seconds_to_string(effective_time), "", ""]
+
+    effective_relative = make_effective(relative)
+    saved = relative - time
+    saved_effective = effective_relative - effective_time
+    return [
+        title,
+        seconds_to_string(time),
+        seconds_to_string(saved),
+        format_percent(saved / relative),
+        seconds_to_string(effective_time),
+        seconds_to_string(saved_effective),
+        format_percent(saved_effective / effective_relative),
+    ]
+
+
+def calc_duration(
+    original_seconds: int,
+    buffs: int,
+    construction_buff: int = 0,
+    alliance_help_max: int = 0,
+    alliance_help_minimum_time: int = 60,  # 1 minute
+) -> None:
+    my_effective_time = partial(effective_time, construction_buff, alliance_help_max, alliance_help_minimum_time)
+    table = PrettyTable(
+        [
+            "Type",
+            "Time on Button",
+            "Saved",
+            "Saved %",
+            "Effective time",
+            "Saved effective",
+            "Saved effective %",
+        ]
+    )
+    table.add_row(gen_row("original time", original_seconds, my_effective_time, None))
+
     time = math.ceil(original_seconds / (1 + buffs / 100))
-    saved = original_seconds - time
-    print(
-        f"Time without additional buff:  {seconds_to_string(time)} - saved: {seconds_to_string(saved)} ({format_percent(saved / original_seconds)})"
+    table.add_row(gen_row("without additional buff", time, my_effective_time, original_seconds))
+    table.add_row(
+        gen_row("additional 25% buff", math.ceil(original_seconds / (1 + (buffs + 25) / 100)), my_effective_time, time)
     )
-    time25 = math.ceil(original_seconds / (1 + (buffs + 25) / 100))
-    saved25 = time - time25
-    print(
-        f"Time with additional 25% buff: {seconds_to_string(time25)} - additionally saved: {seconds_to_string(saved25)} ({format_percent(saved25 / time)})"
+    table.add_row(
+        gen_row("additional 50% buff", math.ceil(original_seconds / (1 + (buffs + 50) / 100)), my_effective_time, time)
     )
-    time50 = math.ceil(original_seconds / (1 + (buffs + 50) / 100))
-    saved50 = time - time50
-    print(
-        f"Time with additional 50% buff: {seconds_to_string(time50)} - additionally saved: {seconds_to_string(saved50)} ({format_percent(saved50 / time)})"
-    )
+    print(table)
 
 
-original_time = time_to_seconds(0, 15, 15, 52)
-buffs_percentage = 78
+original_time = time_to_seconds(2, 15, 15, 52)
+buffs_percentage = 88
+construction_buff = time_to_seconds(0, 1, 28, 48)
+alliance_help_max = 23
+alliance_help_minimum_time = time_to_seconds(0, 0, 6, 30)
 
-calc_duration(original_time, buffs_percentage)
+calc_duration(original_time, buffs_percentage, construction_buff, alliance_help_max, alliance_help_minimum_time)
